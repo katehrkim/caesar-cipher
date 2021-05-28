c6640a08900ba62a25f7a30429bbb1286770aed8
Kate Kim <katehrkim@gmail.com>
2021-05-28 13:02:05 -0400

    Kate's python answer

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,23 @@
 def caesar_cipher(string, shift_amount):
+    output = ''
+    for letter in string:
+        if letter.isalpha():
+            int_rep = ord(letter)
+            new_int_rep = int_rep + shift_amount
+            if letter.islower():
+                if new_int_rep < 97:
+                    diff = 97 - new_int_rep
+                    new_int_rep = 123 - diff
+                elif new_int_rep > 122:
+                    diff = new_int_rep - 122
+                    new_int_rep = diff + 96
+            elif letter.isupper():
+                if new_int_rep < 65:
+                    diff = 65 - new_int_rep
+                    new_int_rep = 91 - diff
+                elif new_int_rep > 90:
+                    diff = new_int_rep - 90
+                    new_int_rep = diff + 64
+            letter = chr(new_int_rep)
+        output += letter
+    return output
--- python/caesar_cipher_spec.py
+++ python/caesar_cipher_spec.py
@@ -1,7 +1,18 @@
 # Rewrite this in Unit Test
-
+import unittest
 from caesar_cipher import caesar_cipher
 
-print(caesar_cipher("Boy! What a string!", -5) == "Wjt! Rcvo v nomdib!")
-print(caesar_cipher("Hello zach168! Yes here.", 5) == "Mjqqt efhm168! Djx mjwj.")
-print(caesar_cipher("Hello Zach168! Yes here.", -5) == "Czggj Uvxc168! Tzn czmz.")
+class CaesarCipherTestCase(unittest.TestCase):
+    """Tests for caesar_cipher.py"""
+
+    def test_one(self):
+        self.assertEqual(caesar_cipher("Boy! What a string!", -5), "Wjt! Rcvo v nomdib!")
+
+    def test_two(self):
+        self.assertEqual(caesar_cipher("Hello zach168! Yes here.", 5), "Mjqqt efhm168! Djx mjwj.")
+
+    def test_three(self):
+        self.assertEqual(caesar_cipher("Hello Zach168! Yes here.", -5), "Czggj Uvxc168! Tzn czmz.")
+
+if __name__ == '__main__':
+    unittest.main()
